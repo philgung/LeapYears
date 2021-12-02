25609b2c302fc38d55915dcc665283b854c7ebc0
Philippe Gung <philippe.gung@youscribe.com>
2021-12-02 10:19:36 +0100

    Green Refacto

--- LeapYears/LeapYears.Tests/UnitTest1.cs
+++ LeapYears/LeapYears.Tests/UnitTest1.cs
@@ -6,37 +6,36 @@ namespace LeapYears.Tests
     public class Tests
     {
         [Test]
-        public void Should_return_true_when_isLeapYears_for_2000()
+        public void Should_return_true_when_year_was_divisible_by_400()
         {
             var isLeapYears = IsLeapYears(2000);
 
             Check.That(isLeapYears).IsTrue();
         }
         
-        [Test]
-        public void Should_return_false_when_isLeapYears_for_1700()
+        [TestCase(1700)]
+        [TestCase(1800)]
+        [TestCase(1900)]
+        [TestCase(2100)]
+        public void Should_return_false_when_year_was_divisible_by_100_but_not_by_400(int years)
         {
-            var isLeapYears = IsLeapYears(1700);
+            var isLeapYears = IsLeapYears(years);
 
             Check.That(isLeapYears).IsFalse();
         }
-
-        [Test]
-        public void Should_return_false_when_isLeapYears_for_1800()
-        {
-            var isLeapYears = IsLeapYears(1800);
-
-            Check.That(isLeapYears).IsFalse();
-        }
-
 
         private bool IsLeapYears(int years)
         {
-            if (years == 1700)
+            if (IsDivisibleBy(100, years) && !IsDivisibleBy(400, years))
             {
                 return false;
             }
             return true;
+        }
+
+        private static bool IsDivisibleBy(int divider, int years)
+        {
+            return years % divider == 0;
         }
     }
 }
